8fd942721822c5ffd115ff289edd9321e717bb6f
Hannah Garcia <hannah.kacso@gmail.com>
2024-07-30 00:26:31 +0000

    updated indentations/brackets to make it more readable

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-let article = ['the', 'a', 'an'];
-let adj = ['great', 'big', 'excellent'];
-let noun = ['jogger', 'racoon'];
-let tld = ['.com', '.net', '.us', '.org '];
+let article = ["the", "a", "an"];
+let adj = ["great", "big", "excellent"];
+let noun = ["jogger", "racoon"];
+let tld = [".com", ".net", ".us", ".org "];
 
 let domain = [];
 
 function isVowel(char) {
-    return ['a', 'e', 'i', 'o', 'u'].includes(char.toLowerCase());
+  return ["a", "e", "i", "o", "u"].includes(char.toLowerCase());
 }
 
 for (let i = 0; i < article.length; i++) {
-    for (let j = 0; j < adj.length; j++) {
-        for (let k = 0; k < noun.length; k++) {
-            for (let l = 0; l < tld.length; l++) {
-            // All combinations with "the"
-            domain.push("the" + adj[j] + noun[k] + tld[l]+'\n');
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < tld.length; l++) {
+        // All combinations with "the"
+        domain.push("the" + adj[j] + noun[k] + tld[l] + "\n");
 
-            // Check if the adjective starts with a vowel or consonant
-            if (isVowel(adj[j][0])) {
-                domain.push("an" + adj[j] + noun[k] + tld[l]+'\n');
-            } else {
-                domain.push("a" + adj[j] + noun[k] + tld[l]+'\n');
-            }
+        // Check if the adjective starts with a vowel or consonant
+        if (isVowel(adj[j][0])) {
+          domain.push("an" + adj[j] + noun[k] + tld[l] + "\n");
+        } else {
+          domain.push("a" + adj[j] + noun[k] + tld[l] + "\n");
         }
+      }
     }
-}
+  }
 }
 
-console.log(domain.join(''));
+console.log(domain.join(""));
